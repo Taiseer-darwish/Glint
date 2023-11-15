ad16b361efe9e2a47cefd0072a9ed5dc67f69410
Taiseer-Darwish <taiseerdarwish@gmail.com>
2023-11-15 02:21:43 +0200

    add new edit

---
+++ GLINT-/back-end-g/main.js
@@ -0,0 +1,8 @@
+// get Element From HTML
+let home = document.getElementById("home");
+
+// open new tap when click at home li
+home.addEventListener("click", function() {
+  window.open("file:///G:/project/templet/templet/GLINT-/Glint/index.html", "_blank");
+});
+
